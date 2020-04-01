603a886d75ba3b9fe1b035272f00a0d63452fe0e
Adnan Hakim <adnanhakim007@gmail.com>
2020-04-01 23:02:27 +0530

    Feed screen done

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -7,58 +7,23 @@ class FeedScreen extends StatefulWidget {
 }
 
 class _FeedScreenState extends State<FeedScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 0.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  'Instagram',
-                  style: TextStyle(
-                    fontFamily: 'Billabong',
-                    fontSize: 32.0,
-                  ),
-                ),
-                Row(
-                  children: <Widget>[
-                    IconButton(
-                      icon: Icon(Icons.live_tv),
-                      iconSize: 30.0,
-                      onPressed: () => print('IGTV'),
-                    ),
-                    SizedBox(width: 16.0),
-                    Container(
-                      width: 35.0,
-                      child: IconButton(
-                        icon: Icon(Icons.send),
-                        iconSize: 30.0,
-                        onPressed: () => print('Direct Messages'),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          Container(
-            width: double.infinity,
-            height: 100.0,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: stories.length + 1,
-              itemBuilder: (BuildContext context, int index) {
-                if (index == 0) {
-                  return SizedBox(width: 10.0);
-                }
-                return Container(
-                  margin: EdgeInsets.all(10.0),
-                  width: 60.0,
-                  height: 60.0,
+  Widget _buildPost(int index) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 15.0),
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 5.0),
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                leading: Container(
+                  width: 50.0,
+                  height: 50.0,
                   decoration: BoxDecoration(
                     shape: BoxShape.circle,
                     boxShadow: [
@@ -72,18 +37,203 @@ class _FeedScreenState extends State<FeedScreen> {
                   child: CircleAvatar(
                     child: ClipOval(
                       child: Image(
-                        width: 60.0,
-                        height: 60.0,
-                        image: AssetImage(stories[index - 1]),
+                        width: 50.0,
+                        height: 50.0,
+                        image: AssetImage(posts[index].authorImageUrl),
                         fit: BoxFit.cover,
                       ),
                     ),
                   ),
-                );
-              },
-            ),
+                ),
+                title: Text(
+                  posts[index].authorName,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: Text(
+                  posts[index].timeAgo,
+                ),
+                trailing: IconButton(
+                  icon: Icon(Icons.more_horiz),
+                  color: Colors.black,
+                  onPressed: () => print('More'),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                width: double.infinity,
+                height: 400.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black45,
+                      offset: Offset(0.0, 8.0),
+                      blurRadius: 8.0,
+                    ),
+                  ],
+                  image: DecorationImage(
+                    image: AssetImage(posts[index].imageUrl),
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.favorite_border),
+                          iconSize: 30.0,
+                          onPressed: () => print('Like post'),
+                        ),
+                        Text(
+                          '2,515',
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(width: 20.0),
+                        Row(
+                          children: <Widget>[
+                            IconButton(
+                              icon: Icon(Icons.chat),
+                              color: Colors.black,
+                              iconSize: 30.0,
+                              onPressed: () => print('Comment post'),
+                            ),
+                            Text(
+                              '2,515',
+                              style: TextStyle(
+                                fontSize: 14.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.bookmark_border),
+                      iconSize: 30.0,
+                      onPressed: () => print('Save post'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _buildPosts() {
+    List<Widget> postWidgets = List<Widget>();
+    for (int i = 0; i < posts.length; i++) {
+      postWidgets.add(_buildPost(i));
+    }
+    return postWidgets;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Instagram',
+                    style: TextStyle(
+                      fontFamily: 'Billabong',
+                      fontSize: 32.0,
+                    ),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(Icons.live_tv),
+                        iconSize: 30.0,
+                        onPressed: () => print('IGTV'),
+                      ),
+                      SizedBox(width: 16.0),
+                      Container(
+                        width: 35.0,
+                        child: IconButton(
+                          icon: Icon(Icons.send),
+                          iconSize: 30.0,
+                          onPressed: () => print('Direct Messages'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                physics: BouncingScrollPhysics(),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      width: double.infinity,
+                      height: 100.0,
+                      child: ListView.builder(
+                        physics: BouncingScrollPhysics(),
+                        scrollDirection: Axis.horizontal,
+                        itemCount: stories.length + 1,
+                        itemBuilder: (BuildContext context, int index) {
+                          if (index == 0) {
+                            return SizedBox(width: 10.0);
+                          }
+                          return Container(
+                            margin: EdgeInsets.all(10.0),
+                            width: 60.0,
+                            height: 60.0,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black45,
+                                  offset: Offset(0.0, 2.0),
+                                  blurRadius: 6.0,
+                                ),
+                              ],
+                            ),
+                            child: CircleAvatar(
+                              child: ClipOval(
+                                child: Image(
+                                  width: 60.0,
+                                  height: 60.0,
+                                  image: AssetImage(stories[index - 1]),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    SizedBox(height: 5.0),
+                    Column(
+                      children: _buildPosts(),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
